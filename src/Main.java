6018a5a5261eff7ecd953a374febd6bfbb7c48c5
Onvm2007 <nicks2026@student.cis.edu.hk>
2023-09-12 14:08:55 +0800

    done!

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int resultOne = add(1,2);
+        System.out.println(resultOne);
+
+        String resultTwo = greeting("king");
+        System.out.println(resultTwo);
+
+        int resultThree = add(1,1,1,1);
+        System.out.println(resultThree);
+
+        printMe("amboise");
+
+
+    }
+
+    public static int add(int numOne, int numTwo){
+        return(numOne+ numTwo);
+    }
+
+    public static String greeting(String name){
+        return("Bonjour " + name + "!");
+    }
+
+    public static int add(int numOne, int numTwo, int numThree, int numFour){
+        int number = add(numOne, numTwo);
+        number = add(number,numThree);
+        number = add(number,numFour);
+        return (number);
+
+    }
+
+    public static void printMe(String toBePrinted){
+        System.out.println(toBePrinted);
+
+    }
+
 }
